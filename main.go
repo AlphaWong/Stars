ed8255d0f300b59d4939bad6d26a96e06d656a3a
alphawong <jhexe2002@gmail.com>
2017-11-30 09:45:18 +0000

    UPT: Generate a markdown.

--- main.go
+++ main.go
@@ -5,34 +5,103 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
-	"os"
+	"sort"
 	"strconv"
+	"strings"
 )
 
+const (
+	Others         = "Others"
+	MarkdownHeader = "%s|⭐️|%s\n"
+	MarkdownColumn = "%s|%s|%s\n"
+	MarkdownStar   = "[ [%s](%s) ]"
+)
+
+type Repos struct {
+	URI  string `json:"uri"`
+	Name string `json:"name"`
+}
+
 func main() {
-	var m map[string]int
-	var t []map[string]interface{}
-	response, err := http.Get("https://api.github.com/users/" + "alphawong" + "/starred?page=" + strconv.Itoa(0))
-	if err != nil {
-		fmt.Printf("%s", err)
-		os.Exit(1)
-	} else {
-		defer response.Body.Close()
-		contents, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			fmt.Printf("%s", err)
-			os.Exit(1)
-		}
-		json.NewDecoder(bytes.NewReader(contents)).Decode(&t)
-		for _, v := range t {
-			lang := v["language"].(string)
-			if _, ok := m[lang]; !ok {
-				fmt.Print(ok)
-				m[lang] = 0
-			}
-			m[lang] = m[lang] + 1
-		}
-		fmt.Print(m)
+	m := GetCustomerGithubStars()
+	PrintMarkdownHeader()
+	PrintMarkdownColumn()
+	PrintAsMarkdown(m)
+}
+
+func PrintMarkdownHeader() {
+	fmt.Printf(MarkdownHeader, "Language", "Repos")
+}
+
+func PrintMarkdownColumn() {
+	fmt.Printf(MarkdownColumn, "---", "---", "---")
+}
+
+func PrintAsMarkdown(m map[string][]Repos) {
+	sk := SortMapByKeyAlphabatAsc(m)
+	sort.Strings(sk)
+	for _, v := range sk {
+		fmt.Printf(MarkdownColumn, v, strconv.Itoa(len(m[v])), GetInnerReposStr(m[v]))
 	}
+}
+
+func SortMapByKeyAlphabatAsc(m map[string][]Repos) (s []string) {
+	for k, _ := range m {
+		s = append(s, k)
+	}
+	return
+}
+
+func GetInnerReposStr(r []Repos) (s string) {
+	var ss []string
+	for _, v := range r {
+		ss = append(ss, fmt.Sprintf(MarkdownStar, v.Name, v.URI))
+	}
+	return strings.Join(ss[:], ", ")
+}
+
+func GetCustomerGithubStars() map[string][]Repos {
+	m := make(map[string][]Repos)
+
+	var t []map[string]interface{}
+
+	i := 0
+	for true {
+		response, err := http.Get("https://api.github.com/users/" + "alphawong" + "/starred?page=" + strconv.Itoa(i))
+		if err != nil {
+			log.Fatalf("%s", err)
+		} else {
+			defer response.Body.Close()
+
+			contents, err := ioutil.ReadAll(response.Body)
+			if err != nil {
+				log.Fatalf("%s", err)
+			}
+
+			json.NewDecoder(bytes.NewReader(contents)).Decode(&t)
+			if len(t) == 0 {
+				return m
+			}
+
+			for _, v := range t {
+				lang := Others
+				if nil != v["language"] {
+					lang = v["language"].(string)
+				}
+				r := Repos{
+					Name: v["full_name"].(string),
+					URI:  v["url"].(string),
+				}
+				if _, ok := m[lang]; !ok {
+					m[lang] = []Repos{r}
+				} else {
+					m[lang] = append(m[lang], r)
+				}
+			}
+		}
+		i++
+	}
+	return m
 }
