5606021bf8c4420a88e0df7f57b453c0ad9b6a4e
alpha.wong <alpha.wong@tuta.io>
2021-03-20 20:18:38 +0800

    feat: remove usless log
    Signed-off-by: alpha.wong <alpha.wong@tuta.io>

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 	"sync"
@@ -56,19 +57,9 @@ func validConfig(config *BaseConfig) {
 	err := validate.Struct(config)
 	if nil != err {
 		for _, err := range err.(validator.ValidationErrors) {
-			fmt.Println(err.Namespace())
-			fmt.Println(err.Field())
-			fmt.Println(err.StructNamespace())
-			fmt.Println(err.StructField())
-			fmt.Println(err.Tag())
-			fmt.Println(err.ActualTag())
-			fmt.Println(err.Kind())
-			fmt.Println(err.Type())
-			fmt.Println(err.Value())
-			fmt.Println(err.Param())
-			fmt.Println()
+			log.Printf(err.Error())
 		}
-		panic("Invalid config struct")
+		log.Panicln("Invalid config struct")
 	}
 }
 
